3a53f5de63211c35f2c1203f7e533b598a598dc0
souria <hungphuoc2004@gmail.com>
2026-02-07 03:01:40 +0700

    update request material create

--- Backend/Models/CapstoneSemester9Context.cs
+++ Backend/Models/CapstoneSemester9Context.cs
@@ -353,7 +353,7 @@ public partial class CapstoneSemester9Context : DbContext
             entity.Property(e => e.RoleDescription).HasMaxLength(255);
             entity.HasData(
                 new Role { RoleId = 1, RoleName = "Admin", RoleDescription = "Administrator role" },
-                new Role { RoleId = 2, RoleName = "User", RoleDescription = "User role" });
+                new Role { RoleId = 3, RoleName = "User", RoleDescription = "User role" });
         });
 
         modelBuilder.Entity<StockTake>(entity =>
